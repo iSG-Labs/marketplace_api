8f1b870b02bfd5c2832a95862d74ba5fc2bb5350
thexeromin <thexeromin@gmail.com>
2024-04-03 11:08:09 +0530

    add: status code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthDto } from './dto'
 import { Tokens } from './types'
@@ -8,11 +8,13 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('local/signup')
+    @HttpCode(HttpStatus.CREATED)
     signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
         return this.authService.signupLocal(dto)
     }
 
     @Post('local/signin')
+    @HttpCode(HttpStatus.OK)
     signinLocal(@Body() dto: AuthDto): Promise<Tokens> {
         return this.authService.signinLocal(dto)
     }
