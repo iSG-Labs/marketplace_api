726cab3a1fb0f3ba68b41a8914be1448307380ba
thexeromin <thexeromin@gmail.com>
2024-04-03 10:58:54 +0530

    update: routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,22 +7,22 @@ import { Tokens } from './types'
 export class AuthController {
     constructor(private authService: AuthService) {}
 
-    @Post('/local/signup')
+    @Post('local/signup')
     signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
         return this.authService.signupLocal(dto)
     }
 
-    @Post('/local/signin')
+    @Post('local/signin')
     signinLocal(@Body() dto: AuthDto): Promise<Tokens> {
         return this.authService.signinLocal(dto)
     }
 
-    @Post('/signout')
+    @Post('signout')
     signout() {
         this.authService.signout()
     }
 
-    @Post('/refresh')
+    @Post('refresh')
     refreshToken() {
         this.authService.refreshToken()
     }
